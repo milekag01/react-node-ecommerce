ad135e7417b93ad7314a07e1ea01ca13038c3d9b
milek agrawal <milekag01@gmail.com>
2020-07-30 04:04:48 +0530

    add braintree controller

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
 const categoryRoutes = require('./routes/category');
 const productRoutes = require('./routes/product');
+const braintreeRoutes = require('./routes/braintree');
+
 // app
 const app = express();
 
@@ -34,6 +36,7 @@ app.use('/api',userRoutes);
 app.use('/api',authRoutes);
 app.use('/api',categoryRoutes);
 app.use('/api',productRoutes);
+app.use('/api',braintreeRoutes);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
